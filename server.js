99370b163542a003cd040519538ba399d9d78e98
yasha6081 <yasha6081@gmail.con>
2020-11-12 10:05:50 -0600

    adding path to seeds

--- server.js
+++ server.js
@@ -5,3 +5,14 @@ const morgan = require("morgan");
 
 const mongoose = require("mongoose");
 
+//express
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+// db mongo
+var MONGODB_URI = process.env.MONGODB_URI || 8080;
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useFindAndModify: false})
+
+
