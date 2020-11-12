6ef40db49ff09d45bfe756c5810e8a04aec4e0a2
yasha6081 <yasha6081@gmail.con>
2020-11-12 10:11:03 -0600

    adding express and port

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-
 const express = require("express");
-
 const morgan = require("morgan");
-
 const mongoose = require("mongoose");
+const app = express();
 
+const PORT = process.env.PORT || 8000;
 //express
 app.use(express.json());
 app.use(express.static('public'));
-
+app.use(morgan("dev"));
 
 
 // db mongo
-var MONGODB_URI = process.env.MONGODB_URI || 8080;
+let MONGODB_URI = process.env.MONGODB_URI || 8080;
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useFindAndModify: false})
 
+//routes
+//require();
+//require();
 
+///listener
+app.listen(PORT, (err) =>{
+      console.log(err);
+  });
