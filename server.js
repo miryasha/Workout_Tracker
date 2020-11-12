c2c0b9c631fd751c58ce8572aa92d21fa1daeeef
yasha6081 <yasha6081@gmail.con>
2020-11-12 11:22:38 -0600

    fixing port number!

--- server.js
+++ server.js
@@ -13,11 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
-
-
 // db mongo
-const MONGODB_URI = process.env.MONGODB_URI || 8080 || "mongodb://blog:lsdkf679ktYh@test-shard-00-00.iffsj.mongodb.net:27017,test-shard-00-01.iffsj.mongodb.net:27017,test-shard-00-02.iffsj.mongodb.net:27017/note-tuts?ssl=true&replicaSet=atlas-pl2ua8-shard-0&authSource=admin&retryWrites=true&w=majority"; ;
+const MONGODB_URI = process.env.MONGODB_URI || 8080 || "mongodb://blog:lsdkf679ktYh@test-shard-00-00.iffsj.mongodb.net:27017,test-shard-00-01.iffsj.mongodb.net:27017,test-shard-00-02.iffsj.mongodb.net:27017/note-tuts?ssl=true&replicaSet=atlas-pl2ua8-shard-0&authSource=admin&retryWrites=true&w=majority" || "mongodb://localhost/workout" ;
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false })
 .then((result) =>{
       ///listener
