72fe4d18025f2fe10c38b8804b536613fe87338f
yasha6081 <yasha6081@gmail.con>
2020-11-12 10:33:21 -0600

    fixing Schemas

--- models/schema.js
+++ models/schema.js
@@ -1,1 +1,55 @@
 const mongoose = require("mongoose");
+
+
+const Schema = mongoose.Schema;
+
+
+//schema instructor
+const workoutsSchema = new Schema(
+      {
+            day: {
+                  type: Date,
+                  default: () => new Date()
+            },
+            exercises: [
+                  {
+                        type: {
+                              type: String,
+                              trim: true,
+                              required:true
+                        },
+                        name: {
+                              type: String,
+                              trim: true,
+                              required:true
+                        },
+                        duration: {
+                              type: Number,
+                              required:true
+                        },
+                        weight: {
+                              type: Number
+                        },
+                        reps: {
+                              type: Number
+                        },
+                        sets: {
+                              type: Number
+                        },
+                        distance: {
+                              type: Number
+                        }
+                  }
+            ]
+      },
+
+      {timestamps: true}
+
+);
+
+
+//remember the name shoud be singular it
+//looks for plural inside the MondoDB
+const Workout = mongoose.model("Workout", workoutsSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -7,18 +7,21 @@ const PORT = process.env.PORT || 8000;
 //express
 app.use(express.json());
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 
 // db mongo
-let MONGODB_URI = process.env.MONGODB_URI || 8080;
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useFindAndModify: false})
-
-//routes
-//require();
-//require();
-
-///listener
+const MONGODB_URI = process.env.MONGODB_URI || 8080 || "mongodb://blog:lsdkf679ktYh@test-shard-00-00.iffsj.mongodb.net:27017,test-shard-00-01.iffsj.mongodb.net:27017,test-shard-00-02.iffsj.mongodb.net:27017/note-tuts?ssl=true&replicaSet=atlas-pl2ua8-shard-0&authSource=admin&retryWrites=true&w=majority"; ;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then((result) =>{
+      ///listener
 app.listen(PORT, (err) =>{
       console.log(err);
   });
+})
+.catch((err) => console.log(err))
+
+//routes
+require("./routes/api")//(app);
+require("./routes/html")//(app);
